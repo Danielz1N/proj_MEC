9cce9b9d8c76a99d3550ac904474e44b93eb1aa0
Danielz1N <silva.daniel86@Gmail.com>
2018-09-12 14:07:30 -0300

    att

--- bd_aval.sql
+++ bd_aval.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 10-Ago-2018 às 03:49
+-- Generation Time: 12-Set-2018 às 19:04
 -- Versão do servidor: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -33,6 +33,17 @@ CREATE TABLE `campus` (
   `nome` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Extraindo dados da tabela `campus`
+--
+
+INSERT INTO `campus` (`id`, `nome`) VALUES
+(1, 'Crateús'),
+(2, 'Fortaleza'),
+(3, 'Quixadá'),
+(4, 'Russas'),
+(5, 'Sobral');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +56,103 @@ CREATE TABLE `curso` (
   `id_unid_acad` bigint(20) NOT NULL,
   `id_campus` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `curso`
+--
+
+INSERT INTO `curso` (`id`, `nome`, `id_unid_acad`, `id_campus`) VALUES
+(1, 'Ciência da Computação', 1, 1),
+(2, 'Engenharia Ambiental', 1, 1),
+(3, 'Engenharia Civil', 1, 1),
+(4, 'Engenharia de Minas', 1, 1),
+(5, 'Sistemas de Informação', 1, 1),
+(6, 'Administração', 2, 2),
+(7, 'Agronomia', 3, 2),
+(8, 'Arquitetura e Urbanismo', 4, 2),
+(9, 'Biblioteconomia', 5, 2),
+(10, 'Biotecnologia', 6, 2),
+(11, 'Ciência da Computação', 6, 2),
+(12, 'Ciências Ambientais', 7, 2),
+(13, 'Ciências Atuariais', 2, 2),
+(14, 'Ciências Biológicas', 6, 2),
+(15, 'Ciências Contábeis', 2, 2),
+(16, 'Ciências Econômicas', 2, 2),
+(17, 'Ciências Sociais', 5, 2),
+(18, 'Cinema e Audiovisual', 8, 2),
+(19, 'Comunicação Social - Jornalismo', 8, 2),
+(20, 'Comunicação Social - Publicidade e Propaganda', 8, 2),
+(21, 'Dança', 8, 2),
+(22, 'Design', 4, 2),
+(23, 'Design - Moda', 8, 2),
+(24, 'Direito', 9, 2),
+(25, 'Economia Ecológica', 3, 2),
+(26, 'Educação Física', 10, 2),
+(27, 'Enfermagem', 11, 2),
+(28, 'Engenharia Ambiental', 4, 2),
+(29, 'Engenharia Civil', 4, 2),
+(30, 'Engenharia de Alimentos', 3, 2),
+(31, 'Engenharia de Computação', 4, 2),
+(32, 'Engenharia de Energias Renováveis', 4, 2),
+(33, 'Engenharia de Pesca', 3, 2),
+(34, 'Engenharia de Petróleo', 4, 2),
+(35, 'Engenharia de Produção Mecânica', 4, 2),
+(36, 'Engenharia de Telecomunicações', 4, 2),
+(37, 'Engenharia Elétrica', 4, 2),
+(38, 'Engenharia Mecânica', 4, 2),
+(39, 'Engenharia Metalúrgica', 4, 2),
+(40, 'Engenharia Química', 4, 2),
+(41, 'Estatística', 6, 2),
+(42, 'Farmácia', 11, 2),
+(43, 'Filosofia', 8, 2),
+(44, 'Finanças', 2, 2),
+(45, 'Física', 6, 2),
+(46, 'Fisioterapia', 12, 2),
+(47, 'Gastronomia', 8, 2),
+(48, 'Geografia', 6, 2),
+(49, 'Geologia', 6, 2),
+(50, 'Gestão de Políticas Públicas', 3, 2),
+(51, 'História', 5, 2),
+(52, 'Letras (Espanhol)', 5, 2),
+(53, 'Letras (Inglês)', 5, 2),
+(54, 'Letras (Língua Portuguesa)', 5, 2),
+(55, 'Letras (Português e Alemão)', 5, 2),
+(56, 'Letras (Português e Espanhol)', 5, 2),
+(57, 'Letras (Português e Francês)', 5, 2),
+(58, 'Letras (Português e Inglês)', 5, 2),
+(59, 'Letras (Português e Italiano)', 5, 2),
+(60, 'Matemática', 6, 2),
+(61, 'Matemática Industrial', 6, 2),
+(62, 'Medicina', 12, 2),
+(63, 'Música', 8, 2),
+(64, 'Oceanografia', 7, 2),
+(65, 'Odontologia', 11, 2),
+(66, 'Pedagogia', 13, 2),
+(67, 'Psicologia', 5, 2),
+(68, 'Química', 6, 2),
+(69, 'Secretariado Executivo', 2, 2),
+(70, 'Sistemas e Mídias Digitais', 14, 2),
+(71, 'Teatro', 8, 2),
+(72, 'Zootecnia', 3, 2),
+(73, 'Ciência da Computação', 15, 3),
+(74, 'Design Digital', 15, 3),
+(75, 'Engenharia de Computação', 15, 3),
+(76, 'Engenharia de Software', 15, 3),
+(77, 'Redes de Computadores', 15, 3),
+(78, 'Sistemas de Informação', 15, 3),
+(79, 'Ciência da Computação', 16, 4),
+(80, 'Engenharia Civil', 16, 4),
+(81, 'Engenharia de Produção', 16, 4),
+(82, 'Engenharia de Software', 16, 4),
+(83, 'Engenharia Mecânica', 16, 4),
+(84, 'Ciências Econômicas', 17, 5),
+(85, 'Engenharia de Computação', 17, 5),
+(86, 'Engenharia Elétrica', 17, 5),
+(87, 'Finanças', 17, 5),
+(88, 'Medicina', 17, 5),
+(89, 'Música', 17, 5),
+(90, 'Odontologia', 17, 5),
+(91, 'Psicologia', 17, 5);
 
 -- --------------------------------------------------------
 
@@ -129,7 +237,7 @@ CREATE TABLE `dimensao_3` (
   `id_avaliacao` bigint(20) NOT NULL,
   `id_campus` bigint(20) NOT NULL,
   `id_unid_acad` bigint(20) NOT NULL,
-  `id_curso` bigint(20) NOT NULL,
+  `id_curso` int(11) NOT NULL,
   `D3.1` int(1) NOT NULL,
   `D3.2` int(1) NOT NULL,
   `D3.3` int(1) NOT NULL,
@@ -179,6 +287,29 @@ CREATE TABLE `unid_acad` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Extraindo dados da tabela `unid_acad`
+--
+
+INSERT INTO `unid_acad` (`id`, `nome`, `id_campus`) VALUES
+(1, 'Campus da UFC em Crateús', 1),
+(2, 'FEAAC', 2),
+(3, 'Centro de Ciências Agrárias', 2),
+(4, 'Centro de Tecnologia', 2),
+(5, 'Centro de Humanidades', 2),
+(6, 'Centro de Ciências', 2),
+(7, 'LABOMAR', 2),
+(8, 'ICA', 2),
+(9, 'Faculdade de Direito', 2),
+(10, 'IEFES', 2),
+(11, 'FFOE', 2),
+(12, ' Faculdade de Medicina', 2),
+(13, 'Faculdade de Educação', 2),
+(14, 'Instituto UFC Virtual', 2),
+(15, 'Campus da UFC em Quixadá', 3),
+(16, 'Campus da UFC em Russas', 4),
+(17, 'Campus da UFC em Sobral', 5);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -203,26 +334,7 @@ ALTER TABLE `curso`
 ALTER TABLE `dimensao_1`
   ADD PRIMARY KEY (`id_avaliacao`,`id_campus`,`id_unid_acad`,`id_curso`),
   ADD KEY `id_campus` (`id_campus`),
-  ADD KEY `id_unid_acad` (`id_unid_acad`),
-  ADD KEY `id_curso` (`id_curso`);
-
---
--- Indexes for table `dimensao_2`
---
-ALTER TABLE `dimensao_2`
-  ADD PRIMARY KEY (`id_avaliacao`,`id_campus`,`id_unid_acad`,`id_curso`),
-  ADD KEY `id_campus` (`id_campus`),
-  ADD KEY `id_unid_acad` (`id_unid_acad`),
-  ADD KEY `id_curso` (`id_curso`);
-
---
--- Indexes for table `dimensao_3`
---
-ALTER TABLE `dimensao_3`
-  ADD PRIMARY KEY (`id_avaliacao`,`id_campus`,`id_unid_acad`,`id_curso`),
-  ADD KEY `id_campus` (`id_campus`),
-  ADD KEY `id_unid_acad` (`id_unid_acad`),
-  ADD KEY `id_curso` (`id_curso`);
+  ADD KEY `id_unid_acad` (`id_unid_acad`);
 
 --
 -- Indexes for table `instru_avaliacao`
@@ -245,13 +357,13 @@ ALTER TABLE `unid_acad`
 -- AUTO_INCREMENT for table `campus`
 --
 ALTER TABLE `campus`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
 
 --
 -- AUTO_INCREMENT for table `instru_avaliacao`
@@ -263,7 +375,7 @@ ALTER TABLE `instru_avaliacao`
 -- AUTO_INCREMENT for table `unid_acad`
 --
 ALTER TABLE `unid_acad`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
@@ -282,26 +394,7 @@ ALTER TABLE `curso`
 ALTER TABLE `dimensao_1`
   ADD CONSTRAINT `dimensao_1_ibfk_1` FOREIGN KEY (`id_avaliacao`) REFERENCES `instru_avaliacao` (`id`),
   ADD CONSTRAINT `dimensao_1_ibfk_2` FOREIGN KEY (`id_campus`) REFERENCES `campus` (`id`),
-  ADD CONSTRAINT `dimensao_1_ibfk_3` FOREIGN KEY (`id_unid_acad`) REFERENCES `unid_acad` (`id`),
-  ADD CONSTRAINT `dimensao_1_ibfk_4` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`);
-
---
--- Limitadores para a tabela `dimensao_2`
---
-ALTER TABLE `dimensao_2`
-  ADD CONSTRAINT `dimensao_2_ibfk_1` FOREIGN KEY (`id_avaliacao`) REFERENCES `instru_avaliacao` (`id`),
-  ADD CONSTRAINT `dimensao_2_ibfk_2` FOREIGN KEY (`id_campus`) REFERENCES `campus` (`id`),
-  ADD CONSTRAINT `dimensao_2_ibfk_3` FOREIGN KEY (`id_unid_acad`) REFERENCES `unid_acad` (`id`),
-  ADD CONSTRAINT `dimensao_2_ibfk_4` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`);
-
---
--- Limitadores para a tabela `dimensao_3`
---
-ALTER TABLE `dimensao_3`
-  ADD CONSTRAINT `dimensao_3_ibfk_1` FOREIGN KEY (`id_avaliacao`) REFERENCES `instru_avaliacao` (`id`),
-  ADD CONSTRAINT `dimensao_3_ibfk_2` FOREIGN KEY (`id_campus`) REFERENCES `campus` (`id`),
-  ADD CONSTRAINT `dimensao_3_ibfk_3` FOREIGN KEY (`id_unid_acad`) REFERENCES `unid_acad` (`id`),
-  ADD CONSTRAINT `dimensao_3_ibfk_4` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`);
+  ADD CONSTRAINT `dimensao_1_ibfk_3` FOREIGN KEY (`id_unid_acad`) REFERENCES `unid_acad` (`id`);
 
 --
 -- Limitadores para a tabela `unid_acad`
--- php-insert.php
+++ php-insert.php
@@ -4,48 +4,55 @@ if(isset($_POST['insert']))
 	$hostname = "localhost";
 	$username = "root";
 	$password = "";
-	$databaseName = "test";
+	$databaseName = "bd_aval";
 
-	$d1_1= $_POST['d1_1'];
-	$d1_2= $_POST['d1_2'];
-	$d1_3= $_POST['d1_3'];
-	$d1_4= $_POST['d1_4'];
-	$d1_5= $_POST['d1_5'];
-	$d1_6= $_POST['d1_6'];
-	$d1_7= $_POST['d1_7'];
-	$d1_8= $_POST['d1_8'];
-	$d1_9= $_POST['d1_9'];
-	$d1_10= $_POST['d1_10'];
-	$d1_11= $_POST['d1_11'];
-	$d1_12= $_POST['d1_12'];
-	$d1_13= $_POST['d1_13'];
-	$d1_14= $_POST['d1_14'];
-	$d1_15= $_POST['d1_15'];
-	$d1_16= $_POST['d1_16'];
-	$d1_17= $_POST['d1_17'];
-	$d1_18= $_POST['d1_18'];
-	$d1_19= $_POST['d1_19'];
-	$d1_20= $_POST['d1_20'];
-	$d1_21= $_POST['d1_21'];
-	$d1_22= $_POST['d1_22'];
-	$d1_23= $_POST['d1_23'];
-	$d1_24= $_POST['d1_24'];
-	$d1_25= $_POST['d1_25'];
-	$d1_26= $_POST['d1_26'];
-	$d1_27= $_POST['d1_27'];
+	$d1_1= $_POST['d1_1'];$d1_2= $_POST['d1_2'];$d1_3= $_POST['d1_3'];$d1_4= $_POST['d1_4'];
+	$d1_5= $_POST['d1_5'];$d1_6= $_POST['d1_6'];$d1_7= $_POST['d1_7'];$d1_8= $_POST['d1_8'];
+	$d1_9= $_POST['d1_9'];$d1_10= $_POST['d1_10'];$d1_11= $_POST['d1_11'];$d1_12= $_POST['d1_12'];
+	$d1_13= $_POST['d1_13'];$d1_14= $_POST['d1_14'];$d1_15= $_POST['d1_15'];$d1_16= $_POST['d1_16'];
+	$d1_17= $_POST['d1_17'];$d1_18= $_POST['d1_18'];$d1_19= $_POST['d1_19'];$d1_20= $_POST['d1_20'];
+	$d1_21= $_POST['d1_21'];$d1_22= $_POST['d1_22'];$d1_23= $_POST['d1_23'];$d1_24= $_POST['d1_24'];
+	$d1_25= $_POST['d1_25'];$d1_26= $_POST['d1_26'];$d1_27= $_POST['d1_27'];
 
+	$d2_1= $_POST['d2_1'];$d2_2= $_POST['d2_2'];$d2_3= $_POST['d2_3'];$d2_4= $_POST['d2_4'];
+	$d2_5= $_POST['d2_5'];$d2_6= $_POST['d2_6'];$d2_7= $_POST['d2_7'];$d2_8= $_POST['d2_8'];
+	$d2_9= $_POST['d2_9'];$d2_10= $_POST['d2_10'];$d2_11= $_POST['d2_11'];$d2_12= $_POST['d2_12'];
+	$d2_13= $_POST['d2_13'];$d2_14= $_POST['d2_14'];$d2_15= $_POST['d2_15'];$d2_16= $_POST['d2_16'];
+	$d2_17= $_POST['d2_17'];$d2_18= $_POST['d2_18'];$d2_19= $_POST['d2_19'];$d2_20= $_POST['d2_20'];
+
+	$d3_1= $_POST['d3_1'];$d3_2= $_POST['d3_2'];$d3_3= $_POST['d3_3'];$d3_4= $_POST['d3_4'];
+	$d3_5= $_POST['d3_5'];$d3_6= $_POST['d3_6'];$d3_7= $_POST['d3_7'];$d3_8= $_POST['d3_8'];
+	$d3_9= $_POST['d3_9'];$d3_10= $_POST['d3_10'];$d3_11= $_POST['d3_11'];$d3_12= $_POST['d3_12'];
+	$d3_13= $_POST['d3_13'];$d3_14= $_POST['d3_14'];$d3_15= $_POST['d3_15'];$d3_16= $_POST['d3_16'];
+	$d3_17= $_POST['d3_17'];$d3_18= $_POST['d3_18'];$d3_19= $_POST['d3_19'];$d3_20= $_POST['d3_20'];
+	$d3_21= $_POST['d3_21'];$d3_22= $_POST['d3_22'];
+
+	$descricao = $_POST['descricao']
+	$data_criacao = $_POST['data']
 
 
 
 	$connect = mysqli_connect($hostname,$username,$password,$databaseName);
-	$query = "INSERT INTO `eoq`(`1`) VALUES ('$d1_1'),('$d1_2'),('$d1_3'),('$d1_4'),('$d1_5'),('$d1_6'),('$d1_7'),('$d1_8'),('$d1_9'),('$d1_10'),('$d1_11'),('$d1_12'),('$d1_13'),('$d1_14'),('$d1_15'),('$d1_16'),('$d1_17'),('$d1_18'),('$d1_19'),('$d1_20'),('$d1_21'),('$d1_22'),('$d1_23'),('$d1_24'),('$d1_25'),('$d1_26'),('$d1_27')";
 
-	$result = mysqli_query($connect,$query);
+	$query = "INSERT INTO `instru_avaliacao`(`descricao`, `data_criacao`) VALUES ('$descricao','$data_criacao')"
+	$id_aval = "SELECT `id` FROM `instru_avaliacao` WHERE descricao='$descricao' AND data_criacao='$data_criacao'"
+	
+	$query1 = "INSERT INTO `dimensao_1`(`id_avaliacao`,`D1.1`, `D1.2`, `D1.3`, `D1.4`, `D1.5`, `D1.6`, `D1.7`, `D1.8`, `D1.9`, `D1.10`, `D1.11`, `D1.12`, `D1.13`, `D1.14`, `D1.15`, `D1.16`, `D1.17`, `D1.18`, `D1.19`, `D1.20`, `D1.21`, `D1.22`, `D1.23`, `D1.24`, `D1.25`, `D1.26`, `D1.27`) VALUES ('$id_aval','$d1_1','$d1_2','$d1_3','$d1_4','$d1_5','$d1_6','$d1_7','$d1_8','$d1_9','$d1_10','$d1_11','$d1_12','$d1_13','$d1_14','$d1_15','$d1_16','$d1_17','$d1_18','$d1_19','$d1_20','$d1_21','$d1_22','$d1_23','$d1_24','$d1_25','$d1_26','$d1_27')"
 
-	if ($result) {
+	$query2 = "INSERT INTO `dimensao_2`(`id_avaliacao`,`D2.1`, `D2.2`, `D2.3`, `D2.4`, `D2.5`, `D2.6`, `D2.7`, `D2.8`, `D2.9`, `D2.10`, `D2.11`, `D2.12`, `D2.13`, `D2.14`, `D2.15`, `D2.16`, `D2.17`, `D2.18`, `D2.19`, `D2.20`) VALUES ('$id_aval','$d2_1','$d2_2','$d2_3','$d2_4','$d2_5','$d2_6','$d2_7','$d2_8','$d2_9','$d2_10','$d2_11','$d2_12','$d2_13','$d2_14','$d2_15','$d2_16','$d2_17','$d2_18','$d2_19','$d2_20')"
+
+	$query3 = "INSERT INTO `dimensao_3`(`id_avaliacao`,`D3.1`, `D3.2`, `D3.3`, `D3.4`, `D3.5`, `D3.6`, `D3.7`, `D3.8`, `D3.9`, `D3.10`, `D3.11`, `D3.12`, `D3.13`, `D3.14`, `D3.15`, `D3.16`, `D3.17`, `D3.18`, `D3.19`, `D3.20`, `D3.21`, `D3.22`) VALUES ('$id_aval','$d3_1','$d3_2','$d3_3','$d3_4','$d3_5','$d3_6','$d3_7','$d3_8','$d3_9','$d3_10','$d3_11','$d3_12','$d3_13','$d3_14','$d3_15','$d3_16','$d3_17','$d3_18','$d3_19','$d3_20','$d3_21','$d3_22')"
+
+	
+
+	$result1 = mysqli_query($connect,$query1);
+	$result2 = mysqli_query($connect,$query2);
+	$result3 = mysqli_query($connect,$query3);
+
+	if ($result1&&$result2&&$result3) {
 		echo "Dados inseridos";
 		echo '<meta http-equiv="refresh" content="8;URL=cadastro.html" />';
-	}else{
+	}else{	
 		echo "Dados não inseridos";
 		echo '<meta http-equiv="refresh" content="8;URL=cadastro.html" />';
 	}
